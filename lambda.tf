8bf2b8f6aa114f3d3262ce95431872e0622d5dd5
Andrew McDonald <amcdonald@hashicorp.com>
2021-11-18 15:57:48 +1100

    update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -12,22 +12,22 @@ variable "aws_region" {
 #}
 
 data "archive_file" "lambda_zip" {
-    type          = "zip"
-    source_file   = "lambda_handler.py"
-    output_path   = "lambda_function.zip"
+  type        = "zip"
+  source_file = "lambda_handler.py"
+  output_path = "lambda_function.zip"
 }
 
 resource "aws_lambda_function" "test_lambda" {
   filename         = "lambda_function.zip"
   function_name    = "lambda_handler"
-  role             = "${aws_iam_role.iam_for_lambda_tf.arn}"
+  role             = aws_iam_role.iam_for_lambda_tf.arn
   handler          = "lambda_handler.lambda_handler"
-  source_code_hash = "${data.archive_file.lambda_zip.output_base64sha256}"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   runtime          = "python3.8"
 }
 
 resource "aws_iam_role" "iam_for_lambda_tf" {
-  name = "iam_for_lambda_tf"
+  name               = "iam_for_lambda_tf"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
