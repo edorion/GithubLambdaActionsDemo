dc61a416d97d321f3adaa2a43a86a79a333d25c7
Andrew McDonald <amcdonald@hashicorp.com>
2021-11-24 11:31:47 +1100

    trying a different connection method

--- lambda_handler.py
+++ lambda_handler.py
@@ -8,14 +8,17 @@ def lambda_handler(event, context):
     dataOld = json.load(f)
     f.close()
 
-
+    print(message)
+    url = f'http://localhost:8200/v1/sys/health'
+    status = urllib.request.urlopen(url).read()
+    print(status)
 #set current datetime as a kv val
 
     now = datetime.now()
     date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
     payload = '{"date":' + date_time + '}"'
 
-    http = urllib3.PoolManager()
+#    http = urllib3.PoolManager()
 #    date_update = http.request('PUT', "http://127.0.0.1:8200/v1/pipeline/lambda/data", data=json.dumps(payload))
     #date_update = requests.put("https://127.0.0.1:8200/v1/pipeline/lambda/data", data=json.dumps(payload))
 #    f = open('/tmp/vault_secret.json',)
@@ -24,17 +27,17 @@ def lambda_handler(event, context):
 
 #re reade datetime kv val
 #    date_update = http.request('GET', "http://127.0.0.1:8200/v1/pipeline/lambda/data")
-    date_update = http.request('GET', "http://127.0.0.1:8200/v1/sys/health")
+#    date_update = http.request('GET', "http://127.0.0.1:8200/v1/sys/health")
     #date_update = requests.get("https://127.0.0.1:8200/v1/pipeline/lambda/data")
-    f = open('/tmp/vault_secret.json',)
-    dataNew = json.load(f)
-    f.close()
+#    f = open('/tmp/vault_secret.json',)
+#    dataNew = json.load(f)
+#    f.close()
 
     return {
         'statusCode': 200,
         'message': message,
-        'request_id': data[request_id],
+        'request_id': data['request_id'],
         'dateOld': dataOld['data']['date'],
 #        'dataChange': dataChange.status_code,
-        'dateNew': dataNew['data']['date']
+#        'dateNew': dataNew['data']['date']
     }
